b46dd736c5a56a2138543c1d0e46b8a7702132a3
euresa <eure.samuel@gmail.com>
2021-05-19 10:32:00 -0400

    latest run results

--- Python Projects/Wikipedia_Scraping/random_walk.py
+++ Python Projects/Wikipedia_Scraping/random_walk.py
@@ -10,20 +10,25 @@ from random import shuffle
 
 from wiki import WikiScraper, combine
 
-N = 100
+N = 1000 
 new_subject = WikiScraper(subject='World History')
 old_links = combine(new_subject.hyperlinks)
 f = open("history.txt", "w")
 for _ in range(N):
     sub = new_subject.subject
-    print(sub)
+    
+
     f.write(sub)
+
+    f.write(', ')
+
+    f.write( str(len(old_links)))
     f.write('\n')
     shuffle(old_links)
     idx = 0
     new_subject = WikiScraper(link=old_links[idx])
     links = combine(new_subject.hyperlinks)
-    while len(links) < 3:
+    while len(links) < 10:
         print(f'{new_subject.subject} was too short with {len(links)}')
         idx = idx + 1
         new_subject = WikiScraper(link=old_links[idx])
