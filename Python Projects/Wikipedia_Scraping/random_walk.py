427d2d5d5dca81a7715463d7a1706187b272b8aa
Sam Eure <sameure@Sams-Air.lan>
2021-06-04 21:34:30 -0400

    making my code more Pythonic

--- Python Projects/Wikipedia_Scraping/random_walk.py
+++ Python Projects/Wikipedia_Scraping/random_walk.py
@@ -10,27 +10,30 @@ from random import shuffle
 
 from wiki import WikiScraper, combine
 
-N = 1000
+N = 10
 new_subject = WikiScraper(subject="World History")
 old_links = combine(new_subject.hyperlinks)
-f = open("history.txt", "w")
-for _ in range(N):
-    sub = new_subject.subject
-    f.write(sub)
-    f.write(", ")
-    f.write(str(len(old_links)))
-    f.write("\n")
-    shuffle(old_links)
-    idx = 0
-    try:
-        new_subject = WikiScraper(link=old_links[idx])
-        links = combine(new_subject.hyperlinks)
-        while len(links) < 10:
-            print(f"{new_subject.subject} was too short with {len(links)}")
-            idx = idx + 1
+file_path = "history.txt"
+with open(file_path, "w") as open_file:
+    for _ in range(N):
+        sub = new_subject.subject
+        print(sub, end='\n')
+        open_file.write(sub)
+        open_file.write(", ")
+        open_file.write(str(len(old_links)))
+        open_file.write("\n")
+        shuffle(old_links)
+        idx = 0
+        try:
             new_subject = WikiScraper(link=old_links[idx])
             links = combine(new_subject.hyperlinks)
-        old_links = links.copy()
-    except:
-        print(f"Failed to scrape page:{old_links[idx]}")
-        idx = idx + 1
+            while len(links) < 10:
+                print(f"{new_subject.subject} was too short with {len(links)}")
+                idx = idx + 1
+                new_subject = WikiScraper(link=old_links[idx])
+                links = combine(new_subject.hyperlinks)
+            old_links = links.copy()
+        except:
+            print(f"Failed to scrape page:{old_links[idx]}")
+            idx = idx + 1
+exit()
