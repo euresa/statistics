7f27ff0b16c96e9c643a4a1b006b6b807fee78dd
Sam Eure <sameure@Sams-MacBook-Air.local>
2021-06-07 14:42:37 -0400

    I mades some updates to wiki_scraper and the wiki module. I also took the nlp code out of the wiki module and put it in a local_wiki module.

---
+++ Python Projects/Wikipedia_Scraping/local_nlp.py
@@ -0,0 +1,66 @@
+"""
+Various functions for performing natural language processing with
+spaCy objects.
+"""
+import pandas # type: ignore
+
+
+def _remove_punctuation(words: list) -> list:
+    """Removes punctuation and special characters."""
+    punc_list = [
+        ".",
+        ",",
+        ")",
+        "(",
+        "/",
+        "]",
+        "[",
+        "\n",
+        " ",
+        ";",
+        ":",
+        '"',
+        "'",
+        "\n \n ",
+        "-",
+    ]
+    no_punc = [w for w in words if w not in punc_list]
+    return no_punc
+
+
+def _get_top_n_strings(str_list: list, n: int=3) -> list:
+    """Finds most popular n strings in a string list. Returns a list of tuples (word, count)."""
+    word_df: pandas.DataFrame = pandas.DataFrame({"words": str_list})
+    word_vc = word_df.value_counts()
+    top_strings = [(word_vc.index[i][0], word_vc[i]) for i in range(n)]
+    return top_strings
+
+
+def _remove_stop_words(doc, lemmas: bool=False) -> list:
+    """Removes "stop words" (common words like "is", "but", "and") from list of words."""
+    if lemmas:
+        # Lemmas are the base form of a word. Ex: the lemma of swimming is swim.
+        interesting_words = [token.lemma_ for token in doc if not token.is_stop]
+    else:
+        interesting_words = [token.text for token in doc if not token.is_stop]
+    return interesting_words
+
+
+def find_popular_words(nlp, words_list: list, n: int=3, lemmas: bool=False) -> list:
+    """Finds the most popular words that aren't stop words in a list of words."""
+    text = " ".join(words_list)
+    doc = nlp(text.lower())
+    nice_words: list = _remove_stop_words(doc, lemmas=lemmas)
+    actual_nice_words: list = _remove_punctuation(nice_words)
+    popular_words: list = _get_top_n_strings(actual_nice_words, n=n)
+    return popular_words
+
+
+def find_top_words(nlp, df, n: int=1, lemmas: bool=False) -> pandas.Series:
+    """Returns a pandas.Series object of tuples comprised of the the top 'n' words 
+    from each row of a pandas.DataFrame already containing a 'words' feature.
+    """
+    top_words: pandas.Series = df.apply(
+        lambda row: find_popular_words(nlp, row.words, n=n, lemmas=lemmas), axis=1
+    )
+    return top_words
--- Python Projects/Wikipedia_Scraping/wiki.py
+++ Python Projects/Wikipedia_Scraping/wiki.py
@@ -1,39 +1,39 @@
 import re
-
 import requests
 
-import pandas as pd
-from bs4 import BeautifulSoup
-
+import pandas as pd  # type: ignore
+from bs4 import BeautifulSoup  # type: ignore
 
 class WikiScraper:
-    """A class used for scraping Wikipedia page data. Organizes natural language on page and hyperlinks."""
-    def __init__(self, subject=None, link=None, printing=False):
-        self.hyperlinks = []
-        self.para_list = []
-        self.section_headers = []
-        if subject is not None:
-            self.HTML_LINK = _make_wiki_link(subject)
+    """A class used for scraping Wikipedia page data. Organizes natural language on
+    a Wikipedia page and creates a list of the hyperlinks present on the page."""
+
+    def __init__(self, subject: str="", link: str="", printing: bool=False) -> None:
+        self.hyperlinks: list = []
+        self.para_list: list = []
+        self.section_headers: list = []
+        if subject != "":
+            self.HTML_LINK: str = _make_wiki_link(subject)
             self._get_wiki_data(self.HTML_LINK, printing=printing)
             self._get_wiki_df()
-        elif link is not None:
+        elif link != "":
             self.HTML_LINK = link
             self._get_wiki_data(self.HTML_LINK, printing=printing)
             self._get_wiki_df()
         else:
             print("Please provide a 'subject' or 'link' argument.")
 
-    def _get_wiki_data(self, HTML_LINK, printing=False):
+    def _get_wiki_data(self, HTML_LINK: str, printing: bool=False) -> None:
         """Takes in a Wikipedia link and returns the natural language paragraphs,
         section titles, and hyperlinks found in the document.
         """
         if self.section_headers == []:
-            soup = _get_soup_doc(HTML_LINK)
-            headers = _get_headers(soup)
+            soup: BeautifulSoup = _get_soup_doc(HTML_LINK)
+            headers: list = _get_headers(soup)
             header_strings = [str(h) for h in headers]
-            h_indices = _get_indices(soup, header_strings)
+            h_indices: list = _get_indices(soup, header_strings)
 
-            self.subject = _get_title(soup)
+            self.subject: str = _get_title(soup)
             printer_count = 5  # counter in the for loop below
             if printing:
                 print("PAGE TITLE:", self.subject)
@@ -41,7 +41,7 @@ class WikiScraper:
 
             for i, head in enumerate(headers):
                 self.section_headers.append(head.text)
-                paragraphs = _get_raw_paragraphs(soup, h_indices, i)
+                paragraphs: list = _get_raw_paragraphs(soup, h_indices, i)
                 hrefs = [_get_hyperlink(p) for p in paragraphs]
                 paras = [_remove_HTML(p) for p in paragraphs]
                 self.hyperlinks.append(combine(hrefs))
@@ -74,115 +74,134 @@ class WikiScraper:
         self.df = wiki_df[~(wiki_df.paragraphs_count == 0)]
 
 
-def _make_wiki_link(subject):
+def _make_wiki_link(subject: str) -> str:
+    """Takes in a string containing a subject (e.g. Python, Cats, Russia, etc.) and
+    returns the related Wikipedia page link to the subject.
+    
+        example:
+        >>> _make_wiki_link('Statistics')
+        https://en.wikipedia.org/wiki/Statistics
+    """
     link = "https://en.wikipedia.org/wiki/" + subject
     return link
 
 
-def _get_soup_doc(HTML_LINK, parser="html.parser"):
+def _get_soup_doc(HTML_LINK: str, parser: str="html.parser") -> BeautifulSoup:
     """Takes in an html link and returns a BeautifulSoup document."""
     response = requests.get(HTML_LINK)
     soup_doc = BeautifulSoup(response.content, "html.parser")
     return soup_doc
 
 
-def _get_title(soup_doc):
+def _get_title(soup_doc: BeautifulSoup) -> str:
     """Returns the title of the web page."""
     title = soup_doc.find(id="firstHeading").text
     return title
 
 
-def _get_headers(soup):
+def _get_headers(soup: BeautifulSoup) -> list:
     """Returns the header of each section in a Wikipedia page."""
     headers = soup.find_all("span", attrs="mw-headline")
     return headers
 
 
-def _remove_footnotes(text):
+def _remove_footnotes(text: str) -> str:
     """Drop footnote superscripts in brackets"""
     text = re.sub(r"\[.*?\]+", "", text)
     return text
 
 
-def _get_indices(soup, string_elements):
+def _get_indices(soup: BeautifulSoup, string_elements: list) -> list:
     """Returns the a list of the starting index for each element in a list of strings."""
     soup_string = str(soup)
     indices = [soup_string.index(string) for string in string_elements]
     return indices
 
 
-def _get_index(text, element):
-    """Attempts to get the index of an element. Returns None if not present"""
+def _get_index(text: str, element: str) -> int:
+    """Attempts to get the index of an element. Returns -1 if not present
+
+        example:
+            >>> _get_index("hello world", "w")
+            6
+            >>> _get_index("hello world", "z")
+            -1
+    """
     try:
         idx = text.index(element)
         return idx
     except:
-        return None
+        return -1
 
 
 def combine(lists):
-    """Combines a list of lists into one list to return."""
+    """Combines a list of lists into one list to return.
+
+        example:
+        >>>combine([[1, 2, 3], [4, 5, 6], [7], [8, 9]])
+        [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    """
     combo = []
     for list_ in lists:
         combo.extend(list_)
     return combo
 
 
-def _collect_pattern_pairs(text, start_char, end_char):
+def _collect_pattern_pairs(text: str, start_char: str, end_char: str) -> list:
     """Returns a list of all text between sets of start_char and end_char characters."""
     collection = []
-    while _get_index(text, start_char) is not None:
-        start = _get_index(text, start_char)
-        end = _get_index(text[start:], end_char) + start  # make sure end is after start
+    while _get_index(text, start_char) != -1:
+        start: int = _get_index(text, start_char)
+        end: int = _get_index(text[start:], end_char) + start  # make sure end is after start
         collection.append(text[start : end + len(end_char)])
         text = text[end + len(end_char) :]
     return collection
 
 
-def _get_raw_paragraphs(soup, h_indices, i):
+def _get_raw_paragraphs(soup: BeautifulSoup, h_indices: list, i: int) -> list:
     """Returns unprocessed HTML paragraphs."""
     soup_string = str(soup)
     try:
         raw_text_i = soup_string[h_indices[i] : h_indices[i + 1]]
     except:
         raw_text_i = soup_string[h_indices[i] :]
-    paragraphs = _collect_pattern_pairs(raw_text_i, "<p>", "</p>")
+    paragraphs: list = _collect_pattern_pairs(raw_text_i, "<p>", "</p>")
     return paragraphs
 
 
-def _get_paragraphs(soup, h_indices, i):
+def _get_paragraphs(soup: BeautifulSoup, h_indices: list, i: int) -> list:
     """Returns the text paragraphs associated with the section header specified by an index."""
-    paragraphs = _get_raw_paragraphs(soup, h_indices, i)
+    paragraphs: list = _get_raw_paragraphs(soup, h_indices, i)
     clean_paragraphs = [_remove_HTML(p) for p in paragraphs]
     return clean_paragraphs
 
 
-def _remove_pattern_pair(text, start_char, end_char):
+def _remove_pattern_pair(text: str, start_char: str, end_char: str) -> str:
     """Removes all text between the start_char and end_char and returns remaining text."""
-    while _get_index(text, start_char) is not None:
-        start = _get_index(text, start_char)
-        end = _get_index(text[start:], end_char) + start  # make sure end is after start
+    while _get_index(text, start_char) != -1:
+        start: int = _get_index(text, start_char)
+        end: int = _get_index(text[start:], end_char) + start  # make sure end is after start
         text = text[:start] + text[end + len(end_char) :]
     return text
 
 
-def _remove_substrings(text, substring_list):
+def _remove_substrings(text: str, substring_list: list) -> str:
     """Removes all occurences of all substrings in a list from a text string. Returns new string."""
     for pattern in substring_list:
         text = re.sub(pattern, "", text)
     return text
 
 
-def _remove_HTML(text):
+def _remove_HTML(text: str) -> str:
     """Removes the HTML elements from a substring of an HTML document and returns resulting string."""
     to_remove = ["</a>", "</sup>", "<p>", "</p>"]
     text = _remove_substrings(text, to_remove)
-    plain_text = _remove_pattern_pair(text, "<", ">")
+    plain_text: str = _remove_pattern_pair(text, "<", ">")
     plain_text = _remove_footnotes(plain_text)
     return plain_text
 
 
-def _clean_wiki_links(hlinks):
+def _clean_wiki_links(hlinks: list) -> list:
     """Completes hyperlinks to other Wikipedia pages."""
     for i, link in enumerate(hlinks):
         if "/wiki/" in link:
@@ -191,16 +210,15 @@ def _clean_wiki_links(hlinks):
     return hlinks
 
 
-def _remove_internal_links(hlinks):
+def _remove_internal_links(hlinks: list) -> list:
     """Removes links that reference different parts of the Wikipedia page."""
-    internal_links = []
     for link in hlinks:
         if "#" == link[0]:
             hlinks.remove(link)
     return hlinks
 
 
-def _clean_cite_notes(hlinks):
+def _clean_cite_notes(hlinks: list) -> list:
     """Removes references to links cited at the bottom of the Wikipedia page."""
     cite_notes = []
     for i, link in enumerate(hlinks):
@@ -211,7 +229,7 @@ def _clean_cite_notes(hlinks):
     return hlinks
 
 
-def _clean_hyperlinks(html_hyperlinks):
+def _clean_hyperlinks(html_hyperlinks: list) -> list:
     """Removes the HTML markup around hyperlinks and returns a list of hyperlinks"""
     to_remove = ['<a href="', '">']
     hlinks = [_remove_substrings(link, to_remove) for link in html_hyperlinks]
@@ -221,89 +239,25 @@ def _clean_hyperlinks(html_hyperlinks):
     return hlinks
 
 
-def _get_hyperlink(text):
+def _get_hyperlink(text: str) -> list:
     """Returns a list of all hyperlinks included in a subsection of an HTML document."""
-    html_hyperlinks = _collect_pattern_pairs(text, "<a href=", ">")
-    hyperlinks = _clean_hyperlinks(html_hyperlinks)
+    html_hyperlinks: list = _collect_pattern_pairs(text, "<a href=", ">")
+    hyperlinks: list = _clean_hyperlinks(html_hyperlinks)
     return hyperlinks
 
 
-def _show_some_text(text):
+def _show_some_text(text: str) -> str:
     """Returns first 100 characters in string."""
     return text[:80] + "..."
 
 
-def _join_strings(string_list, join_char=" \n "):
-    """Joins a list of strings into one string."""
-    return join_char.join(string_list)
-
-
-def _get_words_from_paragraphs(p_list):
+def _get_words_from_paragraphs(p_list: list) -> list:
     """Returns a list of words from a list of paragraphs."""
-    paragraph = _join_strings(p_list)
+    paragraph: str = " \n ".join(p_list)  # paragraphs are separated by ' \n '
     return paragraph.split(" ")
 
 
-def _add_count_feature(df, feature):
+def _add_count_feature(df: pd.DataFrame, feature: str) -> pd.DataFrame:
     """Returns df with new feature that is the length of the list of a different feature."""
     df[feature + "_count"] = df.apply(lambda row: len(row[feature]), axis=1)
     return df
-
-
-def _remove_punctuation(words):
-    """Removes punctuation and special characters."""
-    punc_list = [
-        ".",
-        ",",
-        ")",
-        "(",
-        "/",
-        "]",
-        "[",
-        "\n",
-        " ",
-        ";",
-        ":",
-        '"',
-        "'",
-        "\n \n ",
-        "-",
-    ]
-    no_punc = [w for w in words if w not in punc_list]
-    return no_punc
-
-
-def _get_top_n_strings(str_list, n=3):
-    """Finds most popular n strings in a string list. Returns a list of tuples (word, count)."""
-    word_df = pd.DataFrame({"words": str_list})
-    word_vc = word_df.value_counts()
-    top_strings = [(word_vc.index[i][0], word_vc[i]) for i in range(n)]
-    return top_strings
-
-
-def _remove_stop_words(doc, lemmas=False):
-    """Removes "stop words" (common words like "is", "but", "and") from list of words."""
-    if lemmas:
-        # Lemmas are the base form of a word. Ex: the lemma of swimming is swim.
-        interesting_words = [token.lemma_ for token in doc if not token.is_stop]
-    else:
-        interesting_words = [token.text for token in doc if not token.is_stop]
-    return interesting_words
-
-
-def _find_popular_words(nlp, words_list, n=3, lemmas=False):
-    """Finds the most popular words that aren't stop words in a list of words."""
-    text = _join_strings(words_list, join_char=" ")
-    doc = nlp(text.lower())  # make lowercase
-    nice_words = _remove_stop_words(doc, lemmas=lemmas)
-    actual_nice_words = _remove_punctuation(nice_words)
-    popular_words = _get_top_n_strings(actual_nice_words, n=n)
-    return popular_words
-
-
-def find_top_words(nlp, df, n=1, lemmas=False):
-    """Adds new feature listing tuples of "n" top words."""
-    top_words = df.apply(
-        lambda row: _find_popular_words(nlp, row.words, n=n, lemmas=lemmas), axis=1
-    )
-    return top_words
